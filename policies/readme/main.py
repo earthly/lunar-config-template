d1baf839a6f88bf263372f96dd6802e74504c811
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2025-11-05 19:05:39 -0800

    Update policy to 0.2.1

--- policies/readme/main.py
+++ policies/readme/main.py
@@ -1,4 +1,4 @@
 from lunar_policy import Check
 
 with Check("readme-exists", "Repository should have a README.md file") as c:
-    c.assert_true(c.get(".repo.readme_exists"), "README.md file not found")
+    c.assert_true(c.get_value(".repo.readme_exists"), "README.md file not found")
